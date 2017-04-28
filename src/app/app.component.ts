5b84eea5837b4c6761ff95836df3bb49d72cf5ec
JaeHo Song <thruthesky@JaeHos-MacBook-Pro.local>
2017-04-28 22:18:19 +0800

    update. angular backend component test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-
+import {
+  TestAll,
+  User
+} from 'angular-backend';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  view: string = 'forum-infinite';
+  constructor(
+    public user: User,
+    ta: TestAll ) {
+    ta.backend.setBackendUrl('http://backend.org/index.php');
+    // ta.run();
+  }
+
+
+  onError( str ) {
+    alert( str );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularBackendModule } from 'angular-backend';
+import { AngularBackendComponents } from '../angular-backend-components/angular-backend-components.module';
 
 import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
@@ -12,9 +15,13 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularBackendModule.forRoot(),
+    AngularBackendComponents
   ],
-  providers: [],
+  providers: [ ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
